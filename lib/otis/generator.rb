30107ec8e43ef64463812c97caaf47884b1eccc3
tbueno <tbueno@tbueno.com>
2014-02-24 11:43:16 +0100

    Empty gem

---
+++ lib/otis/generator.rb
@@ -0,0 +1,7 @@
+require "otis/generator/version"
+
+module Otis
+  module Generator
+    # Your code goes here...
+  end
+end
---
+++ lib/otis/generator/version.rb
@@ -0,0 +1,5 @@
+module Otis
+  module Generator
+    VERSION = "0.0.1"
+  end
+end
